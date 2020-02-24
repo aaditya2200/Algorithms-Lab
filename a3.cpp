911fdf16ea252e8ba7614f0448d7a1d276215849
kinglider <aaditya2200@gmail.com>
2020-02-24 11:10:17 +0530

    Add files via upload

--- a3.cpp
+++ a3.cpp
@@ -1,73 +1,70 @@
 #include <iostream>
 using namespace std;
 int i,j;
-class knapsack
+
+int getMax(int arr[], int n) 
+{ 
+    int mx = arr[0]; 
+    for (int i = 1; i < n; i++) 
+        if (arr[i] > mx) 
+            mx = arr[i]; 
+    return mx; 
+} 
+
+
+void countSort(int arr[], int n, int exp) 
+{ 
+    int output[n]; // output array 
+    int i, count[10] = {0}; 
+    for (i = 0; i < n; i++) 
+        count[ (arr[i]/exp)%10 ]++; 
+    for (i = 1; i < 10; i++) 
+        count[i] += count[i - 1]; 
+    for (i = n - 1; i >= 0; i--) 
+    { 
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
+        count[ (arr[i]/exp)%10 ]--; 
+    } 
+    for (i = 0; i < n; i++) 
+        arr[i] = output[i]; 
+} 
+
+void radixsort(int arr[], int n) 
 {
-public:
-int w,p,v;
-float density;
-}k[20];
-
-int returnMAX(knapsack k[],int l)
-{
-	
-	int max = k[0].density;
-	//finding max profit item index
-	for (i = 0; i < l; i++)
-	{
-		if (k[i].density > max && k[i].v != 1)
-		{
-			k[i].v = 1;
-			max = i;
-	
-		}
-	}
-	return max;
-		
-}
-
-void knapsack01(knapsack k[],int l, int capacity)
-{
-	for (i = 0; i < l; i++)
-	{
-		k[i].density = (float)(k[i].w/k[i].p);
-	}
-	int x[l];
-	for (j = 0; j < l;j++)
-		x[j] = 0;
-	for (i = 0; i < l ;i++)
-	{
-		int index = returnMAX(k,l);
-		if (k[index].w <= capacity)
-		{	
-		x[index] = 1;
-		capacity -= k[i].w;
-		}
-	}
-	cout << "\nThe feasible solution is: ";
-	for (i = 0; i < l; i++)
-		cout << x[i] << " ";	
-}
-
-
+    int m = getMax(arr, n); 
+    for (int exp = 1; m/exp > 0; exp *= 10) 
+        countSort(arr, n, exp); 
+} 
 
 int main()
 {
-int l;
-int capacity;
-cout << "\nEnter the number of elements: ";
-cin >> l;
-cout << "\nEnter the following: ";
-for (i = 0; i < l; i++)
-{
-	cout << "\nWeight: ";
-	cin >> k[i].w;
-	cout << "\nProfit: ";
-	cin >> k[i].p;
-	k[i].v = 0;
-}
-cout << "\nEnter capacity: ";
-cin >> capacity;
-knapsack01(k,l,capacity);
-return 0;
+	int n,index = 0;
+	cout << "\nEnter the number of characters: ";
+	cin >> n;
+	int f[n],arr[n];
+	cout << "\nEnter frequency of these characters: ";
+	for (i = 0; i < n; i++)
+	{
+		cin >> f[i];
+	}
+	//performing operation
+	int j = 0;
+	int i =0;
+	while(j<n-1)
+	{
+		f[i+1] = f[i+1] + f[i];
+		arr[index++] = f[i+1];
+		for (int k = 1; k < n; k++)
+			f[k-1] = f[k];
+		n--;
+		radixsort(f,n);
+	}
+	cout << "\nArray: ";
+	for (i = 0; i < n; i++)
+	{
+		cout << f[i] << " ";
+	}
+	for (j = 0; j < index; j++)
+		cout << arr[i] << " ";
+	return 0;
 }
